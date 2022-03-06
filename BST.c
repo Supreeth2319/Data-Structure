980788f527f0b15732661342225a18736d955589
sumukhmg <20ise0038@vvce.ac.in>
2022-03-07 00:10:20 +0530

    added bfs/dfs ,bst and hashing

---
+++ BST.c
@@ -0,0 +1,152 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+struct node
+{
+	int info;
+	struct node *llink;
+	struct node *rlink;
+};
+
+typedef struct node *NODE;
+
+
+NODE insert(int item,NODE root)
+{
+	NODE temp,cur,prev;
+	temp=(struct node*)malloc(sizeof(struct node));
+	temp->info=item;
+	temp->llink=NULL;
+	temp->rlink=NULL;
+	if(root==NULL)
+               {
+		return temp;
+                }
+	prev=NULL;
+	cur=root;
+	while(cur!=NULL)
+	{
+		prev=cur;
+	    	if(item==cur->info)
+		{
+			printf("Duplicate item are not allowed\n");
+			free(temp);
+			return root;
+		}
+		if(item<cur->info)	
+                 cur=cur->llink;
+		else			
+                 cur=cur->rlink;
+	}
+	if(item<prev->info)
+		prev->llink=temp;
+	else
+		prev->rlink=temp;
+	return root;
+}
+
+NODE search(int item, NODE root)
+{
+	NODE cur;
+      cur=root;
+	if(root==NULL)
+      return NULL;
+	while(cur!=NULL)
+	{
+		if(item==cur->info)
+                 {
+                     printf(“key found\n”);
+                      return root;
+                  }              
+		if(item<cur->info)
+			cur=cur->llink;
+		else
+			cur=cur->rlink;
+	}
+      if( cur==NULL)
+      printf(“key not found\n”);
+	return root;
+}
+
+
+
+void inorder(NODE root)
+{
+	if(root==NULL) return;
+	inorder(root->llink);
+	printf("%d\t",root->info);
+	inorder(root->rlink);
+}
+
+void preorder(NODE root)
+{
+	if(root==NULL) return;
+	printf("%d\t",root->info);
+	preorder(root->llink);
+	preorder(root->rlink);
+}
+
+void postorder(NODE root)
+{
+	if(root==NULL) return;
+	postorder(root->llink);
+	postorder(root->rlink);
+	printf("%d\t",root->info)
+
+void main()
+{
+	int item,ch;
+	NODE root=NULL;
+	while (1)
+	{
+printf("\nEnter thechoice\n1.Insert\n2.Search\n3.Inorder\n4.Preorder\n5.Postorder \n6:Exit\n");
+		scanf("%d",&ch);
+		switch(ch)
+		{
+			case 1:
+				printf("Enter the number of items\n");
+				scanf("%d",&n);
+				printf("Enter the item to be inserted\n");
+				root=insert(item,root);
+				break;
+                             case 2 :	
+                                       printf("Enter the item to be searched\n");
+			           scanf("%d",&item);
+				root=search(item,root);
+				break;
+case 3:
+           if(root==NULL)
+				{
+					printf("Tree is empty\n");
+					break;
+				}
+				printf("Inorder traversal is \n");
+				inorder(root);
+				printf("\n");
+				break;
+
+			case 4: if(root==NULL)
+				{
+					printf("Tree is empty\n");
+					break;
+				}
+				printf("Preorder traversal is \n");
+				preorder(root);
+				printf("\n");
+				break;
+			
+			case 5: if(root==NULL)
+				{
+					printf("Tree is empty\n");
+					break;
+				}
+				printf("Postorder traversal is \n");
+				postorder(root);
+				printf("\n");
+				break;
+			
+			case 6: exit(0);
+			default: printf("Enter valid choice\n");
+		}
+	}
+}
---
+++ Hashing.c
@@ -0,0 +1,89 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+#define HASH_SIZE 100
+
+typedef struct employee
+{
+	int	id;
+	char	name[20];
+}EMPLOYEE;
+
+/*Create initial hash table*/
+
+void initialize_hash_table(EMPLOYEE a[])
+{
+	int	i;
+	for(i=0; i<HASH_SIZE; i++)
+	{
+		a[i].id=0;
+	}
+}
+
+/*Compute hash value using the function: H(K)=k%m*/
+
+int H(int k)
+{
+	return k% HASH_SIZE;
+}
+
+
+/*Insert an item into the empty slot using linear probing*/
+
+void insert_table(int id, char name[], EMPLOYEE a[])
+{
+	int	i,index,h_value;
+	h_value= H(id);
+	for(i=0; i<HASH_SIZE; i++)
+	{
+		index=(h_value+i)% HASH_SIZE;
+		if(a[index].id==0)		//empty slot found
+		{
+			a[index].id=id;		//insert employee id
+			strcpy(a[index].name,name);	//insert employee name
+			break;
+		}
+	}
+	if(i==HASH_SIZE) printf("Hash table full\n"); // NO empty slot
+}
+
+/*Display the hash table*/
+void display_Hash_table(EMPLOYEE a[], int n)
+{
+	int 	k,i;
+	printf("H_Value\t Emp_id\t Emp_name\n");
+	for(i=0; i<HASH_SIZE; i++)
+	{
+		If(a[i].id!=0)
+    printf("%d\t %d\t %s\n",i,a[i].id,a[i].name);
+	}
+}
+
+void main()
+{
+	EMPLOYEE a[HASH_SIZE];
+	char	name[20];
+	int	key,id,choice,flag;
+	initialize_hash_table(a);	//Create intial hash table
+	for(;;)
+	{
+		printf("1:Insert 2:Display 4:Exit\n");
+		printf("Enter the choice:");
+		scanf("%d",&choice);
+		switch(choice)
+		{
+			case 1:printf("Enter emp id:");
+				scanf("%d",&id);
+				printf("Enter the name:");
+				scanf("%s",name);
+				insert_hash_table(id,name,a);
+				break;
+			case 2: printf("Contents of hash table\n");
+				display_hash_table(a,HASH_SIZE);
+				printf("\n");
+				break;
+			case 3: exit(0);
+			deafult: printf("Enter valid choice\n");
+		}
+	}
+}
